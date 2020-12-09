65b2cd216731789fc88d1568843a7454ed747fdd
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-09 10:29:33 +0100

    Legg til podlet-dittnav

--- layout.js
+++ layout.js
@@ -6,6 +6,7 @@ const getDecorator = require("./decorator");
 const basePath = process.env.BASE_PATH || "/person/layout-dittnav";
 const port = process.env.PORT || 7000;
 const isDevelopmentEnv = true;
+const dittnavUrl = process.env.DITTNAV_URL || "http://localhost:7300/person/podlet-dittnav/manifest.json";
 const vtaUrl = process.env.VTA_URL || "http://localhost:7100/arbeid/podlet-veientilarbeid/manifest.json";
 const vtaSituasjonUrl = process.env.VTA_SITUASJON_URL || "http://localhost:7200/arbeid/podlet-vta-situasjon/manifest.json";
 
@@ -14,6 +15,13 @@ const layout = new Layout({
   pathname: "/",
   development: isDevelopmentEnv,
   logger: console
+});
+
+const dittnav = layout.client.register({
+  name: "dittnav",
+  uri: dittnavUrl,
+  resolveJs: true,
+  resolveCss: true
 });
 
 const vta = layout.client.register({
@@ -45,6 +53,7 @@ app.get(`${basePath}${layout.pathname()}`,
     Promise.all(
       [
         getDecorator(),
+        dittnav.fetch(incoming),
         vta.fetch(incoming),
         vtaSituasjon.fetch(incoming),
       ]
@@ -55,8 +64,9 @@ app.get(`${basePath}${layout.pathname()}`,
           title: "Dittnav - Layout",
           decorator: result[0],
           podlets: {
-            vta: result[1],
-            vtaSituasjon: result[2],
+            dittnav: result[1],
+            vta: result[2],
+            vtaSituasjon: result[3],
           },
         };
         next();
