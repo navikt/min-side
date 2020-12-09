d713759c197c71120907638308d8b998bbaa90ad
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-09 12:43:09 +0100

    Prettier

--- layout.js
+++ layout.js
@@ -8,34 +8,35 @@ const port = process.env.PORT || 7000;
 const isDevelopmentEnv = true;
 const dittnavUrl = process.env.DITTNAV_URL || "http://localhost:7300/person/podlet-dittnav/manifest.json";
 const vtaUrl = process.env.VTA_URL || "http://localhost:7100/arbeid/podlet-veientilarbeid/manifest.json";
-const vtaSituasjonUrl = process.env.VTA_SITUASJON_URL || "http://localhost:7200/arbeid/podlet-vta-situasjon/manifest.json";
+const vtaSituasjonUrl =
+  process.env.VTA_SITUASJON_URL || "http://localhost:7200/arbeid/podlet-vta-situasjon/manifest.json";
 
 const layout = new Layout({
   name: "layout-dittnav",
   pathname: "/",
   development: isDevelopmentEnv,
-  logger: console
+  logger: console,
 });
 
 const dittnav = layout.client.register({
   name: "dittnav",
   uri: dittnavUrl,
   resolveJs: true,
-  resolveCss: true
+  resolveCss: true,
 });
 
 const vta = layout.client.register({
   name: "vta",
   uri: vtaUrl,
   resolveJs: true,
-  resolveCss: true
+  resolveCss: true,
 });
 
 const vtaSituasjon = layout.client.register({
   name: "vta-situasjon",
   uri: vtaSituasjonUrl,
   resolveJs: true,
-  resolveCss: true
+  resolveCss: true,
 });
 
 const app = express();
@@ -47,18 +48,12 @@ app.set("views", path.resolve(__dirname, "./views/"));
 // isAlive/isReady route for Nais
 app.get(`${basePath}/isAlive|isReady`, (req, res) => res.sendStatus(200));
 
-app.get(`${basePath}${layout.pathname()}`,
+app.get(
+  `${basePath}${layout.pathname()}`,
   async (req, res, next) => {
     const incoming = res.locals.podium;
-    Promise.all(
-      [
-        getDecorator(),
-        dittnav.fetch(incoming),
-        vta.fetch(incoming),
-        vtaSituasjon.fetch(incoming),
-      ]
-    )
-      .then(result => {
+    Promise.all([getDecorator(), dittnav.fetch(incoming), vta.fetch(incoming), vtaSituasjon.fetch(incoming)]).then(
+      (result) => {
         console.log(result);
         res.locals = {
           title: "Dittnav - Layout",
@@ -70,7 +65,8 @@ app.get(`${basePath}${layout.pathname()}`,
           },
         };
         next();
-      });
+      }
+    );
   },
   (req, res) => {
     res.locals.css = layout.client.css();
