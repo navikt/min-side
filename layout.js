53b4a01dfdaad4a72db8626428d8da5a48f440d2
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-12-10 11:00:04 +0100

    La til podlet for dittnav-generelle-fliser

--- layout.js
+++ layout.js
@@ -7,9 +7,9 @@ const basePath = process.env.BASE_PATH || "/person/layout-dittnav";
 const port = process.env.PORT || 7000;
 const isDevelopmentEnv = true;
 const dittnavUrl = process.env.DITTNAV_URL || "http://localhost:7300/person/podlet-dittnav/manifest.json";
+const dittnavGenerelleFliserUrl = process.env.DITTNAV_GENERELLE_FLISER_URL || "http://localhost:7400/person/podlet-dittnav/manifest.json";
 const vtaUrl = process.env.VTA_URL || "http://localhost:7100/arbeid/podlet-veientilarbeid/manifest.json";
-const vtaSituasjonUrl =
-  process.env.VTA_SITUASJON_URL || "http://localhost:7200/arbeid/podlet-vta-situasjon/manifest.json";
+const vtaSituasjonUrl = process.env.VTA_SITUASJON_URL || "http://localhost:7200/arbeid/podlet-vta-situasjon/manifest.json";
 
 const layout = new Layout({
   name: "layout-dittnav",
@@ -22,6 +22,12 @@ const podlets = [
   layout.client.register({
     name: "dittnav",
     uri: dittnavUrl,
+    resolveJs: true,
+    resolveCss: true,
+  }),
+  layout.client.register({
+    name: "dittnav-generelle-fliser",
+    uri: dittnavGenerelleFliserUrl,
     resolveJs: true,
     resolveCss: true,
   }),
