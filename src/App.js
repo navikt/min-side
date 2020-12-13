3f22078c75b98ef6d34cc9919a4471650b1137a5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-12-13 17:26:31 +0100

    EnforceLoginLoader fra nav-dekoratoren-moduler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { EnforceLoginLoader } from "@navikt/nav-dekoratoren-moduler";
+import { isProduction } from "./utils";
 
-const App = () => {
-  return (
-    <div className="App">
-      Hello layout!
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    {isProduction ? <EnforceLoginLoader/> : null}
+  </div>
+);
 
 export default App;
