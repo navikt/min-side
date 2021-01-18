8d955d04a5a6212dfcd0034f04315b43f256ee89
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-01-18 23:11:40 +0100

    Lagt til docker-compose for lokal kjøring

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { EnforceLoginLoader } from "@navikt/nav-dekoratoren-moduler";
-import Panel from "nav-frontend-paneler";
 
-// TODO: wrap with EnforceLoginLoader
-const Login = () => (
-  <Panel border>
-    Login loader
-  </Panel>
-);
+const Login = () => {
+  return <EnforceLoginLoader />
+};
 
 export default Login;
