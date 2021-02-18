9f11c88b8f27f521ad4541cf075da40603738219
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-02-18 14:55:19 +0100

    Revert "La til knapp som tester logging med sentry"
    This reverts commit a5bc0faa40fc216c622768905f2d691f6e7717a4.

--- src/components/LoginLoader.js
+++ src/components/LoginLoader.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { EnforceLoginLoader } from "@navikt/nav-dekoratoren-moduler";
 
 const LoginLoader = () => {
-  return(
-  <React.Fragment>
-    <EnforceLoginLoader />
-    <button onClick={methodDoesNotExist}>Test sentry</button>;
-  </React.Fragment>
-  );
+  return <EnforceLoginLoader />
 };
 
 export default LoginLoader;
