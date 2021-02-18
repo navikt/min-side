cf6e4fe5a94ccabcf2949e82ff627cfb9d620d42
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-02-18 14:08:10 +0100

    La til sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/browser";
 import LoginLoader from "./components/LoginLoader";
 import './css/index.css';
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://ff3cc443e4564378b15df1731ca7861e@sentry.gc.nav.no/68",
+    autoSessionTracking: false,
+  });
+}
 
 ReactDOM.render(
   <LoginLoader />, document.getElementById('dittnav-login-loader')
